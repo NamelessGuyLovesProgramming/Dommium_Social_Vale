499474665b6d0901a39a3ac6d80df047eeed86e2
NamelssGuyLovesProgramming <v.guden@proton.me>
2025-12-15 21:28:52 +0100

    fix: restore ConsiliumSoftware placeholder to unblock build

---
+++ frontend/src/pages/ConsiliumSoftware.jsx
@@ -0,0 +1,14 @@
+import "./ConsiliumSoftware.css"
+
+function ConsiliumSoftware() {
+  return (
+    <div className="page consilium-software-page">
+      <div className="page-container">
+        <h1>Consilium Software</h1>
+        <p>Feature-Seite folgt. Bitte später erneut vorbeischauen.</p>
+      </div>
+    </div>
+  )
+}
+
+export default ConsiliumSoftware
